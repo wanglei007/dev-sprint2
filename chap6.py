71fbf1907ac20409f4ad18b0ab24d6e740bb78da
Lei Wang <lwang@epsilon.com>
2013-10-28 22:36:16 -0400

    my sprint2

--- chap6.py
+++ chap6.py
@@ -1,9 +1,43 @@
 # Enter your answrs for chapter 6 here
-# Name:
+# Name: Lei Wang
 
 
 # Ex. 6.6
 
+def first(word):
+	return word[0]
 
+def last(word):
+	return word[-1]
 
-# Ex 6.8
+def middle(word):
+	return word[1:-1]
+
+def is_palindrome(word):
+	if len(word)==0 or len(word)==1:
+		return True
+	else:
+		if first(word) == last(word):
+			return is_palindrome(middle(word))
+		else:
+			return False
+
+word = raw_input("give me a word: ")
+if is_palindrome(word):
+	print "It is Palidrome!"
+else:
+	print "Sorry, try another one"
+
+# Ex 6.8 GCD
+
+def GCD(a,b):
+	if b==0:
+		return a
+	else:
+		r = a % b
+		return GCD(b, r)
+
+a = raw_input("give me a number: ")
+b = raw_input("give me another number: ")
+print GCD(int(a), int(b))
+
--- chap7.py
+++ chap7.py
@@ -1,9 +1,38 @@
 # Enter your answrs for chapter 7 here
-# Name:
+# Name: Lei Wang
 
 
 # Ex. 7.5
 
+from math import *
+
+def estimate_pi():
+
+	eps = 1e-15
+
+	term = 1
+	myPi=0
+	k=0
+
+	while term>= eps:
+		term = factorial(4*k) * (1103 + 26390 * k)/ float(pow(factorial(k),4) * pow(396, 4*k))
+		myPi = myPi + term
+		print k, term, myPi
+		k= k+1
+
+	myPi = 2 * sqrt(2) / float(9801) * myPi
+
+	myPi = 1/myPi
+
+	print "exiting function with estimation = ", myPi
+
+	return myPi
+
+
+myPi = estimate_pi()
+print "my estimated pi = ", myPi
+
 
 
 # How many iterations does it take to converge?
+# Answer: 4
